a6987fa4468b0b689918fcc056ef53d5512fda91
Ludovic CHEVRAUX <ludovic.chevraux@systemil.net>
2016-08-28 11:51:28 +0200

    add loadAverage function

--- BasicOsCmdParsor.class.php
+++ BasicOsCmdParsor.class.php
@@ -3,23 +3,30 @@
 class BasicOsCmdParsor{
 
   public function df(){
-    $cmd = exec('df', $stdout);
+    exec('df', $stdout);
     $result = array();
     foreach($stdout as $key => $value){
       if($key != 0){
         $dfLine = preg_split('#\s+#', $value);
         $result[$dfLine[0]] = array('fs' => $dfLine[0],
-        'size' => $dfLine[1],
-        'used' => $dfLine[2],
-        'used_percent' => $dfLine[4],
-        'free' => $dfLine[3],
-        'mount_point' => $dfLine[5]
-      );
+          'size' => $dfLine[1],
+          'used' => $dfLine[2],
+          'used_percent' => $dfLine[4],
+          'free' => $dfLine[3],
+          'mount_point' => $dfLine[5]
+        );
+      }
     }
+    return $result;
   }
-  return $result;
-}
 
-
+  public function loadAverage(){
+    exec('uptime', $stdout);
+    $uptimeLine = preg_split('#\s+#', $stdout[0]);
+    $result = array('lavg1m' => rtrim($uptimeLine[8], ','),
+                    'lavg5m' => rtrim($uptimeLine[9], ','),
+                    'lavg15m' => rtrim($uptimeLine[10], ','));
+    return $result;
+  }
 }
 ?>
