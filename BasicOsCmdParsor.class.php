6574113d725e35e2849bd6302992d8e91097b5c3
Ludovic CHEVRAUX <ludovic.chevraux@systemil.net>
2016-08-28 12:32:47 +0200

    Add ps function

--- BasicOsCmdParsor.class.php
+++ BasicOsCmdParsor.class.php
@@ -28,5 +28,34 @@ class BasicOsCmdParsor{
                     'lavg15m' => rtrim($uptimeLine[10], ','));
     return $result;
   }
+
+  public function uptime(){
+    exec('uptime', $stdout);
+    $uptimeLine = preg_split('#\s+#', $stdout[0]);
+    $result = array('uptime' => rtrim($uptimeLine[3], ','));
+    return $result;
+  }
+
+  public function ps(){
+    exec('ps aux', $stdout);
+    $result = array();
+    foreach($stdout as $key => $value){
+      if($key != 0){
+        $psLine = preg_split('#\s+#', $value, 11);
+        $result[$psLine[1]] = array('user' => $psLine[0],
+                          'pid' => $psLine[1],
+                          'cpu_percent' => $psLine[2],
+                          'mem_percent' => $psLine[3],
+                          'vsz' => $psLine[4],
+                          'rss' => $psLine[5],
+                          'tty' => $psLine[6],
+                          'stat' => $psLine[7],
+                          'start' => $psLine[8],
+                          'time' => $psLine[9],
+                          'command' => $psLine[10]);
+      }
+    }
+    return $result;
+  }
 }
 ?>
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BasicOsCmdParsor
 A simple php class to interact with a Linux system and produce a clean result
+
+## Available methods
+* df()
+* loadAverage()
+* uptime()
+* ps()
